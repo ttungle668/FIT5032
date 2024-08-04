ab4872bbc83afc1abe9cb2d58a3c4184a1920d7a
Thanh Tung Le <thanh.le@pointsbet.com>
2024-08-04 22:38:03 +1000

    First Vue Project

---
+++ src/components/ThanhTungLe.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h2>My Name Component</h2>
+      <p>Hello, my name is [ThanhTungLe]!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ThanhTungLe'
+  };
+  </script>
+  
+  <style scoped>
+  h2 {
+    color: blue;
+  }
+  </style>
+  
